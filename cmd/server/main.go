137bbaaaee11a1c7159e137c746b5ba2907d2bb4
rohan <rohanverma031@gmail.com>
2025-04-21 09:06:41 +0530

    feat: removed previous code and added basic config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-
-	"mennr.tech/api/router"
-)
+import "fmt"
 
 func main() {
-	PORT := "8080"
-	if port := os.Getenv("PORT"); port != "" {
-		PORT = port
-	}
 
-	router := router.Router()
-
-	fmt.Println("server is running on port:", PORT)
-	http.ListenAndServe(":"+PORT, router)
-
+	fmt.Println("working")
 }
--- services/responses.go
+++
@@ -1,7 +0,0 @@
-package services
-
-type JSONResponse struct {
-	Error   bool        `json:"error"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-}
